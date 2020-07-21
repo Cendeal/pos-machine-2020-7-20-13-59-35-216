223fb317090213b39f5c62b40a84e2d7a8f146e5
Cendeal <1798062051@qq.com>
2020-07-22 02:36:56 +0800

    finished

---
+++ comtextMapAndPDCD/PDCD.md
@@ -0,0 +1,38 @@
+#### Draw context map
+- P: 15 minutes
+- D: 19 minutes
+- C: Overestimate the planning time
+- A: I think that I need to make a plan time be more closer the fact, don't evaluate too short for myself
+#### write code
+##### printReceipt
+- P: 2 minutes
+- D: 2 minutes
+- C:
+- A:
+##### findAllItemsByBarCodes
+- P: 8 minutes
+- D: 6 minutes
+- C:
+- A:
+##### handleCartItems
+- P: 10 minutes
+- D: 6
+- C:
+- A:
+
+##### generateFormatReceiptDetail
+- P: 5 minutes
+- D: 4 minutes
+- C:
+- A:
+##### calculateItemDetailInfo
+- P: 5 minutes
+- D: 3
+- C:
+- A:
+
+##### calculateTotal
+- P: 4 minutes
+- D: 5 minutes
+- C:
+- A:
--- main.js
+++ main.js
@@ -1,14 +1,95 @@
-function printReceipt(barcodes) {
-    console.log(`
-***<store earning no money>Receipt ***
-Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)
-Name: Sprite, Quantity: 2, Unit price: 3 (yuan), Subtotal: 6 (yuan)
-Name: Battery, Quantity: 1, Unit price: 2 (yuan), Subtotal: 2 (yuan)
-----------------------
-Total: 23 (yuan)
-**********************`)
+const DATA_SOURCE = [
+    {
+        barcode: 'ITEM000000',
+        name: 'Coca-Cola',
+        price: 3
+    },
+    {
+        barcode: 'ITEM000001',
+        name: 'Sprite',
+        price: 3
+    },
+    {
+        barcode: 'ITEM000002',
+        name: 'Apple',
+        price: 5
+    },
+    {
+        barcode: 'ITEM000003',
+        name: 'Litchi',
+        price: 15
+    },
+    {
+        barcode: 'ITEM000004',
+        name: 'Battery',
+        price: 2
+    },
+    {
+        barcode: 'ITEM000005',
+        name: 'Instant Noodles',
+        price: 4
+    }
+]
+
+function printReceipt(barCodes) {
+    const items = findAllItemsByBarCodes(barCodes)
+    const withQuantityItems = handleCartItems(items)
+    console.log(generateFormatReceiptDetail(withQuantityItems))
 }
 
+function findAllItemsByBarCodes(barCodes) {
+    const items = []
+    barCodes.forEach(barcode => {
+        const data = DATA_SOURCE.find(item => {
+            return barcode === item.barcode
+        })
+        if (data) {
+            items.push(data)
+        }
+    })
+    return items
+}
+
+function handleCartItems(items) {
+    let cart = {}
+    items.forEach(item => {
+        if (item.barcode in cart) {
+            cart[item.barcode].quantity++
+        } else {
+            cart[item.barcode] = {
+                info: item,
+                quantity: 1
+            }
+        }
+    })
+    return Object.values(cart)
+}
+
+function generateFormatReceiptDetail(withQuantityItems) {
+    const itemDetails = calculateItemDetailInfo(withQuantityItems)
+    const total = calculateTotal(withQuantityItems)
+    return `
+***<store earning no money>Receipt ***
+${itemDetails.join('\n')}
+----------------------
+Total: ${total} (yuan)
+**********************`
+}
+
+function calculateItemDetailInfo(withQuantityItems) {
+    let itemDetails = []
+    withQuantityItems.forEach(item => {
+        itemDetails.push(`Name: ${item.info.name}, Quantity: ${item.quantity}, Unit price: ${item.info.price} (yuan), Subtotal: ${item.quantity * item.info.price} (yuan)`)
+    })
+    return itemDetails
+
+}
+
+function calculateTotal(withQuantityItems) {
+    return withQuantityItems.reduce((total, currentValue) => {
+        return total + currentValue.quantity * currentValue.info.price
+    }, 0)
+}
 
 module.exports = {
     printReceipt
